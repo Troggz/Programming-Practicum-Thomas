0bbbe0222d14cbaa4c7d5c0acfcbd1602ef80e68
Troggz <thomasnadandraz@gmail.com>
2024-09-26 05:56:36 +0000

    ok

--- Meeting_4/Problem_2/problem_2Test.cpp
+++ Meeting_4/Problem_2/problem_2Test.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+
 using namespace std;
 
 int main(){     
-int input, Num, Output;
-cout<<"Input = ";
-cin>>input;
-cout<<"Output = "<<endl;
-
+int input = 5;
+cout<<"Input = "<<input<<endl;
+int Num;
+string Output;
 
 	for(int i=1;i<=10;i++)
 	{
 	Num = input*i;
 	cout<<input<<" x "<< i <<" = "<<Num<<endl;
+	Output += (to_string(Num)+" ");
+	
     }
-cout<<Num;
+if (Output == "5 10 15 20 25 30 35 40 45 50 "){
+	cout<<"Test Successful"<<endl;
+
+}
+else{
+	cout<<"Test Failed"<<endl;
+}
+
 return 0;
 }
