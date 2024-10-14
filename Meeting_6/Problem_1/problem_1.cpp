6488c20fb1412ba1c51f5509fd100420fb29e7ad
Troggz <thomasnadandraz@gmail.com>
2024-10-14 08:03:17 +0000

    almost there

---
+++ Meeting_6/Problem_1/problem_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct Student { 
+    string NIM; 
+    float UTS;
+    float UAS; 
+};
+
+int n ;
+int main() {  
+    cout<<"Enter the number of students : "; 
+    cin>>n; 
+    cout<<"Data mahasiswa"<<endl; 
+
+Student theStudent[n];
+for (int i=0;i<n;i++) {  
+    cout<<"NIM : "; 
+    cin>>theStudent[i].NIM;   
+    cout<<"Enter UTS value : "; 
+    cin>>theStudent[i].UTS; 
+    cout<<"Enter UTS value : "; 
+    cin>>theStudent[i].UAS; 
+}
+float avg[n];
+for (int i = 0;i < n;i++){
+    avg[i]= (theStudent[i].UTS + theStudent[i].UAS)/2;
+}
+cout<<endl<<"Data Mahasiswa "<<endl<<"===================================="<<endl;
+
+for (int i = 0; i < n ;i++){
+    cout<<i + 1<<"."<<endl;
+    cout<<"NIM = "<<theStudent[i].NIM<<endl;
+    cout<<"Average score = "<<avg[i]<<endl;
+
+}
+
+}
---
+++ Meeting_6/Problem_1/problem_1Test.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct Student { 
+    string NIM; 
+    float UTS;
+    float UAS; 
+};
+
+int n = 3 ;
+
+Student theStudent[n];
+//TEST CASE
+theStudent[0].nim = 542313;
+theStudent[0].UTS = 90;
+theStudent[0].UAS = 95;
+
+theStudent[1].nim = 672912;
+theStudent[1].UTS = 80;
+theStudent[1].UAS = 85;
+
+theStudent[2].nim = 879123;
+theStudent[2].UTS = 65;
+theStudent[2].UAS = 75;
+
+int main() {  
+    cout<<"Enter the number of students : "<<n<<endl;
+    cout<<"Data mahasiswa"<<endl; 
+
+for (int i=0;i<n;i++) {  
+    cout<<"NIM : "; 
+    cout<<theStudent[i].NIM;   
+    cout<<"Enter UTS value : "; 
+    cout<<theStudent[i].UTS; 
+    cout<<"Enter UTS value : "; 
+    cout<<theStudent[i].UAS; 
+}
+float avg[n];
+for (int i = 0;i < n;i++){
+    avg[i]= (theStudent[i].UTS + theStudent[i].UAS)/2;
+}
+cout<<endl<<"Data Mahasiswa "<<endl<<"===================================="<<endl;
+
+for (int i = 0; i < n ;i++){
+    cout<<i + 1<<"."<<endl;
+    cout<<"NIM = "<<theStudent[i].NIM<<endl;
+    cout<<"Average score = "<<avg[i]<<endl;
+
+}
+
+}
